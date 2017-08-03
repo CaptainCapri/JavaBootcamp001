cd8d254f4da2dda8670ad57da68050357444166f
Captain Capri <der.michaelhoffmann@googlemail.com>
2017-08-03 20:13:02 +0200

    primitiveDatatypes

---
+++ primitveDatatypes/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+         char myChar01 = '\u00A9';
+        System.out.println("Unicode output is = " + myChar01);
+
+        boolean myBool = true;
+
+        System.out.println("I am so " + myBool);
+
+        char myChar02 = '\u00AE';
+        System.out.println("Unicode output is = " + myChar02);
+
+
+
+    }
+}
