df990acdc7872ceacaeb6b7ba8e6947fae0af74d
Michael Hoffmann <michael.hoffmann@spar-ics.com>
2018-03-05 16:07:20 +0100

    adds new lectures 006 and 007

--- 0062MethodsEtc/src/com/company/Main.java
+++ 0062MethodsEtc/src/com/company/Main.java
@@ -3,13 +3,15 @@ package com.company;
 public class Main {
     // Overloading Methods
     public static void main(String[] args) {
-        int newScore = calculateScore("Fred", 500);
-        System.out.println("New score is " + newScore);
-        calculateScore(75);
-        calculateScore();
+        //int newScore = calculateScore("Fred", 500);
+        //System.out.println("New score is " + newScore);
+        //calculateScore(75);
+        //calculateScore();
+        //calculateScore("blabla",1);
 
 
-        calculateScore()
+        calcFeetAndInchesToCentimeters(5,7);
+        calcFeetAndInchesToCentimeters(83);
 
     }
 
@@ -33,12 +35,37 @@ public class Main {
     }
 
 
-    public static calcFeetAndInchesToCentimeters(float feet, float inch){
-        if (feet >= 0 || (feet >= 0 && feet < 12)){
+    public static double calcFeetAndInchesToCentimeters(double feet, double inch) {
+        if (feet >= 0 && feet >= 0 && inch <= 12 && inch >= 0) {
+            double i = feet * 30.48;
+            double j = inch * 2.54;
+            i = i + j;
+            System.out.println(feet + "Feet and " + inch + " Inches are " + i + " Centimeters");
+            return i;
 
+        } else {
+            System.out.println("Numbers invalid!");
+            return -1;
         }
-        else {
-            return -1
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inch) {
+        if (inch >= 0) {
+            //double j = inch * 2.54;
+            double i = inch % 12;
+            //i = i * 2.54;
+            double j = Math.floor(inch / 12);
+
+            System.out.println("feet: " + j + " inch: " + i);
+            calcFeetAndInchesToCentimeters(j,i);
+
+
+            return 0;
+
+        } else {
+            System.out.println("Numbers Invalid!");
+            return -1;
         }
 
     }
