10eb283d8f8ed8033fd1300ba5505d52b6e46ce1
Michael Hoffmann <michael.hoffmann@spar-ics.com>
2019-02-05 16:22:45 +0100

    filereader replacer

---
+++ Document_ReadReplacer/src/com/michaelhoffmann/Main.java
@@ -0,0 +1,46 @@
+package com.michaelhoffmann;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<File> filesInFolder = Files.walk(Paths.get("C:\\Spar-Projekte\\JavaBootcamp001\\read_Replacer\\targetFiles"))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+        System.out.println(filesInFolder + "\n");
+        for(int i = 0; i < filesInFolder.size(); i++   ) {
+            System.out.println(filesInFolder.get(i) + "\n");
+            Path path = Paths.get("" + filesInFolder.get(i));
+            Charset charset = StandardCharsets.UTF_8;
+
+            try {
+                String content = new String(Files.readAllBytes(path), charset);
+                content = content.replaceAll("bar", "foo");
+                content = content.replaceAll("rip", "ra");
+
+                Files.write(path, content.getBytes(charset));
+            } catch (IOException exeption001) {
+                System.out.println("there went sth wrong: " + exeption001);
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
+
---
+++ read_Replacer/src/main/java/com/michaelhoffmann/XLStoCSV.java
@@ -0,0 +1,26 @@
+package com.michaelhoffmann;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+
+public class XLStoCSV {
+
+
+
+
+
+
+
+
+
+}
