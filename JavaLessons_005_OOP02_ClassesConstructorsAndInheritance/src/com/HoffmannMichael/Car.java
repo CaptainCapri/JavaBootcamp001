169839b85f3e8d7ae9d4f718a57a9dea591625b7
Captain Capri <der.michaelhoffmann@ghooglemail.com>
2020-08-31 23:13:48 +0200

    Neue Aktualisierung Still OOP

--- JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Car.java
+++ JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Car.java
@@ -8,6 +8,40 @@ public class Car {
   private String engine;
   private String colour;
 
+
+  public int getDoors() {
+    return doors;
+  }
+
+  public void setDoors(int doors) {
+    this.doors = doors;
+  }
+
+  public int getWheels() {
+    return wheels;
+  }
+
+  public void setWheels(int wheels) {
+    this.wheels = wheels;
+  }
+
+  public String getEngine() {
+    return engine;
+  }
+
+  public void setEngine(String engine) {
+    this.engine = engine;
+  }
+
+  public String getColour() {
+    return colour;
+  }
+
+  public void setColour(String colour) {
+    this.colour = colour;
+  }
+
+
   public void setModel(String model) {
     String validModel = model.toLowerCase();
     if (validModel.equals("v8interceptor") || validModel.equals("ibiza")) {
