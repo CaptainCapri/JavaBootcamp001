86fc90f907fdb1ef205cd37e42388816a0e75f82
Captain Capri <der.michaelhoffmann@ghooglemail.com>
2020-10-17 12:45:24 +0200

    Java Bootcamp Programmierübung 32

--- JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Point.java
+++ JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Point.java
@@ -1,25 +1,30 @@
 package com.HoffmannMichael;
 
-public class Point {
+public class Point
+{
 
     private int x;
     private int y;
 
-    public Point(int x, int y) {
+    public Point(int x, int y)
+    {
         this.x = x;
         this.y = y;
     }
 
-    public Point() {
+    public Point()
+    {
         this.x = 0;
         this.y = 0;
     }
 
-    public int getX() {
+    public int getX()
+    {
         return x;
     }
 
-    public void setX(int x) {
+    public void setX(int x)
+    {
         this.x = x;
     }
 
@@ -27,11 +32,27 @@ public class Point {
         return y;
     }
 
-    public void setY(int y) {
+    public void setY(int y)
+    {
         this.y = y;
     }
 
+    public double distance ()
+    {
+        double distance = Math.sqrt((0 - x) * (0 - x) + (0 - y) * (0 - y));
+        return distance;
+    }
 
+    public double distance (int xB, int yB)
+    {
+        double distance = Math.sqrt((xB - x) * (xB - x) + (yB - y) * (yB - y));
+        return distance;
+    }
 
+    public double distance (Point anotherPoint)
+    {
+        double distance = Math.sqrt((anotherPoint.getX() - x) * (anotherPoint.getX() - x) + (anotherPoint.getY() - y) * (anotherPoint.getY() - y));
+        return distance;
+    }
 
 }
