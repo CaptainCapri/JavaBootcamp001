c657e78b376966c4579db9044973b86d88dedf99
Captain Capri <der.michaelhoffmann@ghooglemail.com>
2020-10-17 12:16:29 +0200

    Java Bootcamp Programmierübung 31 und 32

--- JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Main.java
+++ JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Main.java
@@ -77,6 +77,23 @@ public class Main {
         VipCustomer diegosVipAccount = new VipCustomer();
         System.out.println(diegosVipAccount.getName() + diegosVipAccount.getCreditLimit() + diegosVipAccount.geteMail());
 
+
+
+        Wall wall01 = new Wall(5, 4);
+        System.out.println("area= " + wall01.getArea());
+
+        wall01.setHeight(-1.5);
+        System.out.println("width= " + wall01.getWidth());
+        System.out.println("height= " + wall01.getHeight());
+        System.out.println("area= " + wall01.getHeight());
+
+        wall01.setWidth(-1.5);
+        System.out.println("width= " + wall01.getWidth());
+        System.out.println("height= " + wall01.getHeight());
+        System.out.println("area= " + wall01.getHeight());
+
+
+
     }
 
 }
