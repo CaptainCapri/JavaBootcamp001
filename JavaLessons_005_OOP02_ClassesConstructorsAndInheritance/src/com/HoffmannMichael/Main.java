be29ede428ffdcb204b3a8401de231b148ee0a4c
Captain Capri <der.michaelhoffmann@ghooglemail.com>
2020-10-17 17:31:54 +0200

    Java Bootcamp Programmierübung 33 & 34

--- JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Main.java
+++ JavaLessons_005_OOP02_ClassesConstructorsAndInheritance/src/com/HoffmannMichael/Main.java
@@ -41,56 +41,80 @@ public class Main {
 
 
 //     BANK ACCOUNT - CLASS
-        BankAccount HotDocsBankaccount = new BankAccount("35985792357", 0.00, "Hot Doc", "thehottest@doc.sr", "06660815");
+//        BankAccount HotDocsBankaccount = new BankAccount("35985792357", 0.00, "Hot Doc", "thehottest@doc.sr", "06660815");
 //        HotDocsBankaccount.setBalance(0);
 //        HotDocsBankaccount.setCustomerName("Hot Doc");
 //        HotDocsBankaccount.setEmail("theHottest@doc.sr");
 //        HotDocsBankaccount.setPhone("06660815");
 //        HotDocsBankaccount.setNumber("35985792357");
 
-        HotDocsBankaccount.deposit(777);
-        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
-        HotDocsBankaccount.withdraw(333);
-        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
-        HotDocsBankaccount.withdraw(777);
-        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
-        HotDocsBankaccount.withdraw(-12);
-
-        System.out.println("Name: " + HotDocsBankaccount.getCustomerName());
-        System.out.println("Number: " + HotDocsBankaccount.getNumber());
-        System.out.println("Phone: " + HotDocsBankaccount.getPhone());
-        System.out.println("Phone: " + HotDocsBankaccount.getEmail());
-
-
-
-        BankAccount michisAccount = new BankAccount("Michi", "michi@email.com", "0815");
-        System.out.println(michisAccount.getNumber() + " name: " + michisAccount.getCustomerName());
-
-        System.out.println("\n");
-
-        VipCustomer hotDocsVipAccount = new VipCustomer("Hot Doc", 13, "thehottest@doc.sr");
-        System.out.println(hotDocsVipAccount.getName() + hotDocsVipAccount.getCreditLimit() + hotDocsVipAccount.geteMail());
-
-        VipCustomer xargothVipAccount = new VipCustomer("Xargoth", 17000);
-        System.out.println(xargothVipAccount.getName() + xargothVipAccount.getCreditLimit() + xargothVipAccount.geteMail());
-
-        VipCustomer diegosVipAccount = new VipCustomer();
-        System.out.println(diegosVipAccount.getName() + diegosVipAccount.getCreditLimit() + diegosVipAccount.geteMail());
+//        HotDocsBankaccount.deposit(777);
+//        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
+//        HotDocsBankaccount.withdraw(333);
+//        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
+//        HotDocsBankaccount.withdraw(777);
+//        System.out.println("Balance: " + HotDocsBankaccount.getBalance());
+//        HotDocsBankaccount.withdraw(-12);
+//
+//        System.out.println("Name: " + HotDocsBankaccount.getCustomerName());
+//        System.out.println("Number: " + HotDocsBankaccount.getNumber());
+//        System.out.println("Phone: " + HotDocsBankaccount.getPhone());
+//        System.out.println("Phone: " + HotDocsBankaccount.getEmail());
 
 
 
-        Wall wall01 = new Wall(5, 4);
-        System.out.println("area= " + wall01.getArea());
+//        BankAccount michisAccount = new BankAccount("Michi", "michi@email.com", "0815");
+//        System.out.println(michisAccount.getNumber() + " name: " + michisAccount.getCustomerName());
+//
+//        System.out.println("\n");
+//
+//        VipCustomer hotDocsVipAccount = new VipCustomer("Hot Doc", 13, "thehottest@doc.sr");
+//        System.out.println(hotDocsVipAccount.getName() + hotDocsVipAccount.getCreditLimit() + hotDocsVipAccount.geteMail());
+//
+//        VipCustomer xargothVipAccount = new VipCustomer("Xargoth", 17000);
+//        System.out.println(xargothVipAccount.getName() + xargothVipAccount.getCreditLimit() + xargothVipAccount.geteMail());
+//
+//        VipCustomer diegosVipAccount = new VipCustomer();
+//        System.out.println(diegosVipAccount.getName() + diegosVipAccount.getCreditLimit() + diegosVipAccount.geteMail());
 
-        wall01.setHeight(-1.5);
-        System.out.println("width= " + wall01.getWidth());
-        System.out.println("height= " + wall01.getHeight());
-        System.out.println("area= " + wall01.getHeight());
 
-        wall01.setWidth(-1.5);
-        System.out.println("width= " + wall01.getWidth());
-        System.out.println("height= " + wall01.getHeight());
-        System.out.println("area= " + wall01.getHeight());
+
+//        Wall wall01 = new Wall(5, 4);
+//        System.out.println("area= " + wall01.getArea());
+//
+//        wall01.setHeight(-1.5);
+//        System.out.println("width= " + wall01.getWidth());
+//        System.out.println("height= " + wall01.getHeight());
+//        System.out.println("area= " + wall01.getHeight());
+
+//        wall01.setWidth(-1.5);
+//        System.out.println("width= " + wall01.getWidth());
+//        System.out.println("height= " + wall01.getHeight());
+//        System.out.println("area= " + wall01.getHeight());
+
+
+//        Carpet carpet01 = new Carpet(3.5);
+//        Floor floor01 = new Floor(2.75, 4.0);
+//        Calculator calculator = new Calculator(floor01, carpet01);
+//        System.out.println("total: " + calculator.getTotalCost());
+
+//        carpet01 = new Carpet(1.5);
+//        floor01 = new Floor(5.4, 4.5);
+//        calculator = new Calculator(floor01, carpet01);
+//        System.out.println("total: " + calculator.getTotalCost());
+
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+
 
 
 
